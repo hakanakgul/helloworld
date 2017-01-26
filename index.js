98812b2b139c0c5259a32172dce0df0599e61847
Hakan Akgül <hakanakgul34@gmail.com>
2017-01-26 13:31:44 +0300

    deneme

---
+++ index.js
@@ -0,0 +1,59 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var host = "192.168.1.100";
+var port = "80";
+
+http.listen(port, host, function() {
+    console.log('listening on *:80');
+});
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/style.css', function(req, res) {
+    res.sendFile(__dirname + '/style.css');
+});
+app.get('/normalize.css', function(req, res) {
+    res.sendFile(__dirname + '/normalize.css');
+});
+
+
+app.get('/sitelogo', function(req, res) {
+    res.sendFile(__dirname + '/Content/SiteLogo.png');
+});
+app.get('/ytlogo', function(req, res) {
+    res.sendFile(__dirname + '/Content/YoutubeLogo.png');
+});
+app.get('/fblogo', function(req, res) {
+    res.sendFile(__dirname + '/Content/FacebookLogo.png');
+});
+app.get('/instalogo', function(req, res) {
+    res.sendFile(__dirname + '/Content/InstagramLogo.png');
+});
+
+
+
+io.on('connection', function(socket) {
+    console.log(zaman() + ' : a user connected');
+
+    socket.on('disconnect', function() {
+        console.log(zaman() + ' : user disconnected');
+    });
+
+});
+
+function zaman() {
+
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "/" +
+        (currentdate.getMonth() + 1) + "/" +
+        currentdate.getFullYear() + " @ " +
+        currentdate.getHours() + ":" +
+        currentdate.getMinutes() + ":" +
+        currentdate.getSeconds();
+
+    return datetime;
+}
---
+++ node_modules/flexiblegs-scss/README.md
@@ -0,0 +1,26 @@
+# [flexiblegs-scss](http://flexible.gs)
+
+Flexible Grid System Scss
+
+## Install
+
+### [Download](https://raw.githubusercontent.com/flexiblegs/flexiblegs-scss/master/flexiblegs-scss.scss)
+```scss
+@import "flexiblegs-scss";
+```
+
+### [npm](https://www.npmjs.com/package/flexiblegs-scss)
+```
+$ npm install --save flexiblegs-scss
+```
+
+### [bower](http://bower.io/search/?q=flexiblegs-scss)
+```
+$ bower install --save flexiblegs-scss
+```
+
+## License
+- Flexible Grid System is licensed under the MIT license.
+  - [http://opensource.org/licenses/MIT](http://opensource.org/licenses/MIT)
+- Flexible Grid System documentation is licensed under a Creative Commons Attribution 4.0 International License.
+  - [http://creativecommons.org/licenses/by/4.0](http://creativecommons.org/licenses/by/4.0)
